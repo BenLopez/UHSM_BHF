5dc21604dbe7f06435d7d96b10fc7067add0f565
Ben Lopez <btlopez@me.com>
2018-09-26 10:43:50 +0100

    Update to GMM density estimation development script.

--- WaveformCode/MixturesofGMMDevelopmentScriptScript.R
+++ WaveformCode/MixturesofGMMDevelopmentScriptScript.R
@@ -4,7 +4,7 @@ GMMmodelbyPatient <- list()
 
 for(i in 1:dim( DataBaseMaster$NAFPatientsDatabase)[1]){
 TempData1 <- DataBaseMaster$NAFPatientsDatabase[i , DataBaseMaster$NAFPatientsDatabase[i ,  ,1] !=0  ,1:11]
-if(length(TempData1) < 1000){}
+if(length(TempData1) < 1000){next}
 GMMmodelbyPatient[[i]] <- densityMclust( DP_RemoveNaRows( TempData1 ) , G = 100 )
 DP_WaitBar(i/dim(DataBaseMaster$NAFPatientsDatabase)[1])
 }
