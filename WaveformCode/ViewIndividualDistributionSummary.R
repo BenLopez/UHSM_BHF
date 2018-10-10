96bb20d9368c94f5da142086fff0c6b7629344ba
Ben Lopez <btlopez@me.com>
2018-10-10 12:04:20 +0100

    Script to view processed distribution summary data.

---
+++ WaveformCode/ViewIndividualDistributionSummary.R
@@ -0,0 +1,18 @@
+{
+  if(file.exists('CheckforDefaultsScript.R')){
+    source('CheckforDefaultsScript.R')
+  }else{
+    pathFiles <- setwd(paste0(choose.dir(caption="Select folder with source code."), "\\"))
+    source("LibrariesAndSettings.R" , print.eval  = TRUE )
+    DP_LoadPatientIndex()
+    DP_ChooseDataReps()
+    FilestoProcess <- DP_ChooseECGstoProcess() 
+    HoursBeforeandAfter <- DP_SelectHoursBeforeandAfter()
+  }
+  listAllPatients <- DP_FilterPatients(listAllPatients , PatIndex2017 , HowtoFilterops , path , FilestoProcess)
+  set.seed(1)
+}
+
+patient <- DP_choosepatient(listAllPatients = listAllPatients)
+DistributionSummaries = DP_LoadDistributionSummaries(path , patient)
+
